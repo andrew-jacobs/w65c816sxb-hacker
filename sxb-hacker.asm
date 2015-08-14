b4587db526351b537e97f230993d96ec24406bca
andrew-jacobs <andrew.jacobs@btinternet.com>
2015-08-14 15:34:36 +0100

    Initial file import

---
+++ sxb-hacker.asm
@@ -0,0 +1,480 @@
+;==============================================================================
+;  ______  ______        _   _            _
+; / ___\ \/ / __ )      | | | | __ _  ___| | _____ _ __
+; \___ \\  /|  _ \ _____| |_| |/ _` |/ __| |/ / _ \ '__|
+;  ___) /  \| |_) |_____|  _  | (_| | (__|   <  __/ |
+; |____/_/\_\____/      |_| |_|\__,_|\___|_|\_\___|_|
+;
+; A program for Hacking your W65C816SXB
+;------------------------------------------------------------------------------
+; Copyright (C)2015 Andrew Jacobs
+; All rights reserved.
+;
+; This work is made available under the terms of the Creative Commons
+; Attribution-NonCommercial-ShareAlike 4.0 International license. Open the
+; following URL to see the details.
+;
+; http://creativecommons.org/licenses/by-nc-sa/4.0/
+;
+;==============================================================================
+; Notes:
+;
+; This program provides a simple monitor that you can use to inspect the memory
+; in your W65C816SXB and reprogram parts of the flash ROM.
+;
+;------------------------------------------------------------------------------
+
+                pw      132
+                inclist on
+
+                chip    65816
+                longi   off
+                longa   off
+
+                include "w65c816.inc"
+                include "w65c816sxb.inc"
+
+;==============================================================================
+; ASCII Character Codes
+;------------------------------------------------------------------------------
+
+BEL             equ     $07
+BS              equ     $08
+LF              equ     $0a
+CR              equ     $0d
+ESC             equ     $1b
+DEL             equ     $7f
+
+;==============================================================================
+; Data Areas
+;------------------------------------------------------------------------------
+
+                page0
+
+BUFLEN          ds      1
+BANK            ds      1
+
+ADDR_S          ds      3
+ADDR_E          ds      3
+
+TEMP		ds	1
+
+                data
+                org     $200
+
+BUFFER          ds      128
+
+;==============================================================================
+; Initialisation
+;------------------------------------------------------------------------------
+
+                code
+                public  Start
+                extern  UartRx
+                extern  UartTx
+Start:
+                short_a                         ; Configure register sizes
+                long_i
+		jsr	UartCRLF
+                ldx     #TITLE                  ; Display application title
+                jsr     UartStr
+
+                stz     BANK                    ; Reset default bank
+
+;==============================================================================
+; Command Processor
+;------------------------------------------------------------------------------
+
+NewCommand:
+                stz     BUFLEN                  ; Clear the buffer
+                jsr     UartCRLF                ; Move to a new line
+
+                lda     #'.'                    ; Output the prompt
+                jsr     UartTx
+
+                short_i
+                ldx     #0
+DisplayCmd:     cpx     BUFLEN                  ; Any saved characters
+                beq     ReadCommand
+                lda     BUFFER,x                ; Yes, display them
+                jsr     UartTx
+                inx
+                bra     DisplayCmd
+
+RingBell:
+                lda     #BEL                    ; Make a beep
+                jsr     UartTx
+
+ReadCommand:
+                jsr     UartRx                  ; Wait for character
+
+                cmp     #ESC                    ; Cancel input?
+                beq     NewCommand              ; Yes, clear and restart
+                cmp     #CR                     ; End of command?
+                beq     ProcessCommand          ; Yes, start processing
+
+                cmp     #BS                     ; Back space?
+                beq     BackSpace
+                cmp     #DEL                    ; Delete?
+                beq     BackSpace
+
+                cmp     #' '                    ; Printable character
+                bcc     RingBell                ; No.
+                cmp     #DEL
+                bcs     RingBell                ; No.
+                sta     BUFFER,x                ; Save rhe character
+                inx
+                jsr     UartTx                  ; Echo it and repeat
+                bra     ReadCommand
+
+BackSpace:
+                cpx     #0                      ; Buffer empty?
+                beq     RingBell                ; Yes, beep and continue
+                dex                             ; No, remove last character
+                lda     #BS
+                jsr     UartTx
+                lda     #' '
+                jsr     UartTx
+                lda     #BS
+                jsr     UartTx
+                bra     ReadCommand             ; And retry
+
+ProcessCommand:
+                stx     BUFLEN                  ; Save final length
+                ldy     #0                      ; Load index for start
+
+                jsr     SkipSpaces              ; Fetch command character
+                bcs     NewCommand              ; None, empty command
+		
+;==============================================================================
+; B - Select Memory Bank
+;------------------------------------------------------------------------------
+
+		cmp	#'B'			; Select memory bank?
+		bne	NotMemoryBank
+		
+		ldx	#BANK			; Parse bank
+		jsr	GetByte
+		bcc	$+5
+		jmp	ShowError
+		jmp	NewCommand
+NotMemoryBank:
+
+;==============================================================================
+; M - Display Memory
+;------------------------------------------------------------------------------
+
+                cmp     #'M'                    ; Memory display?
+                bne     NotMemoryDisplay
+
+                ldx     #ADDR_S                 ; Parse start address
+                jsr     GetAddr
+                bcc     $+5
+                jmp     ShowError
+                ldx     #ADDR_E                 ; Parse end address
+                jsr     GetAddr
+                bcc     $+5
+                jmp     ShowError
+
+DisplayMemory:
+                jsr     UartCRLF
+                lda     ADDR_S+2                ; Show memory address
+                jsr     UartHex2
+                lda     #':'
+                jsr     UartTx
+                lda     ADDR_S+1
+                jsr     UartHex2
+                lda     ADDR_S+0
+                jsr     UartHex2
+
+                ldy     #0                      ; Show sixteen bytes of data
+ByteLoop:       lda     #' '
+                jsr     UartTx
+                lda     [ADDR_S],y
+                jsr     UartHex2
+                iny
+                cpy     #16
+                bne     ByteLoop
+
+                lda     #' '
+                jsr     UartTx
+                lda     #'|'
+                jsr     UartTx
+                ldy     #0                      ; Show sixteen characters
+CharLoop:       lda     [ADDR_S],Y
+                jsr     IsPrintable
+                bcs     $+4
+                lda     #'.'
+                jsr     UartTx
+                iny
+                cpy     #16
+                bne     CharLoop
+                lda     #'|'
+                jsr     UartTx
+
+                clc                             ; Bump the display address
+                tya
+                adc     ADDR_S+0
+                sta     ADDR_S+0
+                bcc     $+4
+                inc     ADDR_S+1
+                
+                sec                             ; Exceeded the end address?
+                sbc     ADDR_E+0
+                lda     ADDR_S+1
+                sbc     ADDR_E+1
+                bmi     DisplayMemory           ; No, show more
+
+                jmp     NewCommand
+NotMemoryDisplay:
+
+;==============================================================================
+; R - Select ROM Bank
+;------------------------------------------------------------------------------
+
+                cmp     #'R'                    ; ROM Bank?
+                bne     NotROMBank
+		
+		jsr	SkipSpaces
+		bcc	$+5
+BankFail:	jmp	ShowError
+		cmp	#'0'
+		bcc	BankFail
+		cmp	#'3'+3
+		bcs	BankFail
+		
+		sta	TEMP
+		lda	#0
+		ror	TEMP			; Bit 0 set
+		bcs	$+4
+		ora	#%00001100		; No, make CA2 (A15) low
+		ror	TEMP			; Bit 1 set
+		bcs	$+4
+		ora	#%11000000		; No, make CB2 (FAMS) low
+		sta	VIA2_PCR
+		
+		jmp	NewCommand
+NotROMBank:
+
+;==============================================================================
+; X - XMODEM Upload
+;------------------------------------------------------------------------------
+
+                cmp     #'X'                    ; XModem upload?
+                bne     NotXModem
+
+		jmp	NewCommand
+NotXModem:
+
+;==============================================================================
+; ? - Help
+;------------------------------------------------------------------------------
+
+                cmp     #'?'                    ; Help command?
+                bne     NotHelp
+
+                long_i
+                ldx     #HELP                   ; Output help string
+                jsr     UartStr
+                jmp     NewCommand
+NotHelp:
+
+ShowError:
+                long_i
+                ldx     #ERROR                  ; Output error message
+                jsr     UartStr
+                jmp     NewCommand
+
+;==============================================================================
+;------------------------------------------------------------------------------
+
+GetByte:
+                stz     0,x                     ; Set the target address
+                jsr     SkipSpaces              ; Skip to first real characater
+                bcc     $+3
+                rts                             ; None found
+                jsr     IsHexDigit              ; Must have atleast one digit
+                bcc     ByteFail
+                jsr     AddDigit
+                jsr     NextChar
+                bcs     ByteDone
+                jsr     IsHexDigit
+                bcc     ByteDone
+                jsr     AddDigit
+ByteDone:       clc
+                rts
+ByteFail:       sec
+                rts
+
+GetAddr:
+                stz     0,x                     ; Set the target address
+                stz     1,x
+                lda     BANK
+                sta     2,x
+                jsr     SkipSpaces              ; Skip to first real characater
+                bcc     $+3
+                rts                             ; None found
+                jsr     IsHexDigit              ; Must have atleast one digit
+                bcc     AddrFail
+                jsr     AddDigit
+                jsr     NextChar
+                bcs     AddrDone
+                jsr     IsHexDigit
+                bcc     AddrDone
+                jsr     AddDigit
+                jsr     NextChar
+                bcs     AddrDone
+                jsr     IsHexDigit
+                bcc     AddrDone
+                jsr     AddDigit
+                jsr     NextChar
+                bcs     AddrDone
+                jsr     IsHexDigit
+                bcc     AddrDone
+                jsr     AddDigit
+AddrDone:       clc
+                rts
+AddrFail:       sec
+                rts
+
+AddDigit:
+                sec                             ; Convert ASCII to binary
+                sbc     #'0'
+                cmp     #$0a
+                bcc     $+4
+                sbc     #7
+
+                asl     0,x                     ; Shift up one nybble
+                rol     1,x
+                asl     0,x
+                rol     1,x
+                asl     0,x
+                rol     1,x
+                asl     0,x
+                rol     1,x
+
+                ora     0,x                     ; Merge in new digit
+                sta     0,x                     ; Then get next digit
+                rts
+
+; Get the next character from the command buffer updating the position in X.
+; Set the carry if the end of the buffer is reached.
+
+NextChar:
+                cpy     BUFLEN                  ; Any characters left?
+                bcc     $+3
+                rts
+                lda     BUFFER,y
+                iny
+                jmp     ToUpperCase
+
+SkipSpaces:
+                jsr     NextChar                ; Fetch next character
+                bcc     $+3                     ; Any left?
+                rts                             ; No
+                cmp     #' '                    ; Is it a space?
+                beq     SkipSpaces              ; Yes, try again
+                clc
+                rts                             ; Done
+
+; If the character in A is lower case then convert it to upper case.
+
+ToUpperCase:
+                jsr     IsLowerCase             ; Test the character
+                bcc     $+4
+                sbc     #32                     ; Convert lower case
+                clc
+                rts                             ; Done
+
+; Determine if the character in A is a lower case letter. Set the carry if it
+; is, otherwise clear it.
+
+IsLowerCase:
+                cmp     #'a'                    ; Between a and z?
+                bcc     ClearCarry
+                cmp     #'z'+1
+                bcs     ClearCarry
+SetCarry:       sec
+                rts
+ClearCarry:     clc
+                rts
+
+; Determine if the character in A is a hex character. Set the carry if it is,
+; otherwise clear it.
+
+IsHexDigit:
+                cmp     #'0'                    ; Between 0 and 9?
+                bcc     ClearCarry
+                cmp     #'9'+1
+                bcc     SetCarry
+                cmp     #'A'                    ; Between A and F?
+                bcc     ClearCarry
+                cmp     #'F'+1
+                bcc     SetCarry
+                bra     ClearCarry
+
+; Determine if the character in A is a printable character. Set the carry if it
+; is, otherwise clear it.
+
+IsPrintable:
+                cmp     #' '
+                bcc     ClearCarry
+                cmp     #DEL
+                bcc     SetCarry
+                bra     ClearCarry
+
+;==============================================================================
+;------------------------------------------------------------------------------
+
+UartHex2:
+                pha                             ; Save the original byte
+                lsr     a                       ; Shift down hi nybble
+                lsr     a
+                lsr     a
+                lsr     a
+                jsr     UartHex                 ; Display
+                pla                             ; Recover data byte
+
+UartHex:
+                and     #$0f                    ; Strip out lo nybble
+                sed                             ; Convert to ASCII
+                clc
+                adc     #$90
+                adc     #$40
+                cld
+                jmp     UartTx                  ; And display
+
+; Display the string of characters starting a the memory location pointed to by
+; X (16-bits).
+
+UartStr:
+                lda     0,x
+                bne     $+3
+                rts
+                jsr     UartTx
+                inx
+                bra     UartStr
+
+; Display a CR/LF control character sequence.
+
+UartCRLF:
+                jsr     UartCR
+                lda     #LF
+                jmp     UartTx
+UartCR:         lda     #CR
+                jmp     UartTx
+
+;==============================================================================
+;------------------------------------------------------------------------------
+
+TITLE           db      CR,LF,"SXB-Hacker [15.08]",0
+
+ERROR           db      CR,LF,"Error - Type ? for help",0
+
+HELP            db	CR,LF,"B bb           - Set memory bank"
+		db      CR,LF,"M bbbb eeee    - Display memory in current bank"
+		db	CR,LF,"R 0-3          - Select ROM bank 0-3"
+                db      CR,LF,"X bbbb         - XMODEM upload to current bank"
+                db      0
+
+                end
---
+++ w65c816sxb.asm
@@ -0,0 +1,236 @@
+;==============================================================================
+; __        ____  ____   ____ ___  _  __  ______  ______
+; \ \      / / /_| ___| / ___( _ )/ |/ /_/ ___\ \/ / __ )
+;  \ \ /\ / / '_ \___ \| |   / _ \| | '_ \___ \\  /|  _ \
+;   \ V  V /| (_) |__) | |__| (_) | | (_) |__) /  \| |_) |
+;    \_/\_/  \___/____/ \____\___/|_|\___/____/_/\_\____/
+;
+; Basic Vector Handling for the W65C816SXB Development Board
+;------------------------------------------------------------------------------
+; Copyright (C)2015 HandCoded Software Ltd.
+; All rights reserved.
+;
+; This work is made available under the terms of the Creative Commons
+; Attribution-NonCommercial-ShareAlike 4.0 International license. Open the
+; following URL to see the details.
+;
+; http://creativecommons.org/licenses/by-nc-sa/4.0/
+;
+;==============================================================================
+; Notes:
+;
+;
+;------------------------------------------------------------------------------
+
+                pw      132
+                inclist on
+
+                chip    65816
+                longi   off
+                longa   off
+
+                include "w65c816.inc"
+                include "w65c816sxb.inc"
+
+;==============================================================================
+; Power On Reset
+;------------------------------------------------------------------------------
+
+                code
+                extern  Start
+RESET:
+                sei                             ; Stop interrupts
+                ldx     #$ff
+                txs
+
+                lda     #%00011111              ; 8 bits, 1 stop bit, 19200 baud
+                sta     ACIA_CTL
+                lda     #%00001001              ; No parity, no interrupt
+                sta     ACIA_CMD
+                lda     ACIA_RXD                ; Clear receive buffer
+                lda     ACIA_RXD
+
+                native
+                jmp     Start                   ; Jump to the application start
+
+;==============================================================================
+; Interrupt Handlers
+;------------------------------------------------------------------------------
+
+; Handle IRQ and BRK interrupts in emulation mode.
+
+IRQBRK:
+                bra     $                       ; Loop forever
+
+; Handle NMI interrupts in emulation mode.
+
+NMIRQ:
+                bra     $                       ; Loop forever
+
+;------------------------------------------------------------------------------
+
+; Handle IRQ interrupts in native mode.
+
+IRQ:
+                bra     $                       ; Loop forever
+
+; Handle IRQ interrupts in native mode.
+
+BRK:
+                bra     $                       ; Loop forever
+
+; Handle IRQ interrupts in native mode.
+
+NMI:
+                bra     $                       ; Loop forever
+
+;------------------------------------------------------------------------------
+
+; COP and ABORT interrupts are not handled.
+
+COP:
+                bra     $                       ; Loop forever
+
+ABORT:
+                bra     $                       ; Loop forever
+
+;==============================================================================
+; Buffered UART Interface
+;------------------------------------------------------------------------------
+
+; Adds the character in A to the transmit buffer. If the buffer is full then
+; wait for it to drain.
+
+                public  UartTx
+UartTx:
+                if      0
+                phx
+                php
+                short_ai
+                ldx     #$00                    ; Make data port all input
+                stx     VIA2_DDRA
+                sta     VIA2_ORA                ; Save the output character
+                lda     #%01
+TxWait:         bit     VIA2_IRB                ; Is there space for more data
+                bne     TxWait
+
+                lda     VIA2_IRB                ; Strobe WR
+                and     #$fb
+                tax
+                ora     #$04
+                sta     VIA2_ORB
+                lda     #$ff                    ; Make data port all output
+                sta     VIA2_DDRA
+                nop
+                nop
+                stx     VIA2_ORB                ; End strobe
+                lda     VIA2_IRA
+                ldx     #$00                    ; Make data port all output
+                stx     VIA2_DDRA
+                plp
+                plx
+                rts
+                endif
+
+                pha
+                php
+                short_a
+                sta     ACIA_TXD
+                jsr     TxDelay
+                jsr     TxDelay
+                jsr     TxDelay
+                jsr     TxDelay
+                jsr     TxDelay
+                plp
+                pla
+                rts
+
+TxDelay:        lda     #0
+                inc     a
+                bne     $-1
+                rts
+
+; Fetch the next character from the RX buffer waiting for some to arrive if the
+; buffer is empty.
+
+                public  UartRx
+UartRx:
+                if      0
+                phx
+                php
+                short_ai
+                lda     #$02                    ; Wait until data in buffer
+RxWait :        bit     VIA2_IRB
+                bne     RxWait
+
+                lda     VIA2_IRB                ; Strobe /RD low
+                ora     #$08
+                tax
+                and     #$f7
+                sta     VIA2_ORB
+                nop                             ; Wait for data to be available
+                nop
+                nop
+                nop
+                lda     VIA2_IRA                ; Read it
+                stx     VIA2_ORB                ; And end the strobe
+                plp
+                plx
+                rts                             ; Done
+                endif
+
+                php
+                short_a
+                lda     ACIA_SR
+                bpl     $-3
+                lda     ACIA_RXD
+                plp
+                rts
+
+;==============================================================================
+; Reset Vectors
+;------------------------------------------------------------------------------
+
+ShadowVectors   section offset $7ee0
+
+                ds      4               ; Reserved
+                dw      COP             ; $FFE4 - COP(816)
+                dw      BRK             ; $FFE6 - BRK(816)
+                dw      ABORT           ; $FFE8 - ABORT(816)
+                dw      NMI             ; $FFEA - NMI(816)
+                ds      2               ; Reserved
+                dw      IRQ             ; $FFEE - IRQ(816)
+
+                ds      4
+                dw      COP             ; $FFF4 - COP(C02)
+                ds      2               ; $Reserved
+                dw      ABORT           ; $FFF8 - ABORT(C02)
+                dw      NMIRQ           ; $FFFA - NMI(C02)
+                dw      RESET           ; $FFFC - RESET(C02)
+                dw      IRQBRK          ; $FFFE - IRQBRK(C02)
+
+                ends
+
+;------------------------------------------------------------------------------
+
+Vectors         section offset $ffe0
+
+                ds      4               ; Reserved
+                dw      COP             ; $FFE4 - COP(816)
+                dw      BRK             ; $FFE6 - BRK(816)
+                dw      ABORT           ; $FFE8 - ABORT(816)
+                dw      NMI             ; $FFEA - NMI(816)
+                ds      2               ; Reserved
+                dw      IRQ             ; $FFEE - IRQ(816)
+
+                ds      4
+                dw      COP             ; $FFF4 - COP(C02)
+                ds      2               ; $Reserved
+                dw      ABORT           ; $FFF8 - ABORT(C02)
+                dw      NMIRQ           ; $FFFA - NMI(C02)
+                dw      RESET           ; $FFFC - RESET(C02)
+                dw      IRQBRK          ; $FFFE - IRQBRK(C02)
+
+                ends
+
+                end
